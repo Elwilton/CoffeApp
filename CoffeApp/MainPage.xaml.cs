01d9a0c656cf8418425fa6f0a4dfbec160b9449f
elwil <elwil@ELWILTONDPLUS>
2024-04-18 16:37:13 -0300

    Adicionar arquivos de projeto.

---
+++ CoffeApp/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace CoffeApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+
+}
---
+++ CoffeApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CoffeApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
